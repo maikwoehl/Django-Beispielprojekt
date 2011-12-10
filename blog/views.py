4174812cf94340b7050d1466fe097d86cd0e60cf
Maik Wöhl <maik.woehl@web.de>
2011-12-10 10:06:20 +0100

    add all data

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response
+from artikel.models import Artikel
+
+def index(request):
+    try:
+        articles = Artikel.objects.all()
+    except Artikel.DoesNotExist:
+        articles = "No content available!"
+        
+    return render_to_response('index.html', {"articles":articles})
+
